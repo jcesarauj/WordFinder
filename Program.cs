71708cfb3f8a65bf6973485ff1ce2a11939f4425
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2021-08-04 22:46:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WordFinder
+{
+	class Program
+	{
+		public static void Main()
+		{
+			var matrix = new string[] { "chill", "cold", "wind" };
+			var wordstream = new string[] { "abcdc", "fgwio", "chill", "pqnsd", "uvdxy" };
+			var result = new WordFinder(matrix).Find(wordstream);
+			Console.WriteLine(string.Join(",", result));
+		}		
+	}
+}
---
+++ WordFinder.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordFinder
+{
+	public class WordFinder
+	{
+		private readonly HashSet<string> _matrix;
+		private readonly HashSet<string> _resultSet;
+
+		public WordFinder(IEnumerable<string> matrix)
+		{
+			_matrix = new HashSet<string>(matrix);
+			_resultSet = new HashSet<string>();
+		}
+
+		public IEnumerable<string> Find(IEnumerable<string> wordstream)
+		{
+			SearchInLine(wordstream);
+			SearchInColumn(wordstream);
+			return _resultSet.ToList();
+		}
+
+		private void SearchInLine(IEnumerable<string> wordstream)
+		{
+			var lineStringBuilder = new StringBuilder();
+
+			var characterMatrix = wordstream
+				.Select(row => row.ToCharArray())
+				.ToArray();
+
+			for (var i = 0; i < characterMatrix.Length; i++)
+			{
+				lineStringBuilder.Length = 0;
+
+				for (var j = 0; j < characterMatrix[i].Length; j++)
+				{
+					lineStringBuilder.Append(characterMatrix[i][j]);
+				}
+
+				_resultSet.UnionWith(_matrix.Where(searchTerm =>
+												 lineStringBuilder.ToString().Contains(searchTerm)));
+			}
+		}
+
+		private void SearchInColumn(IEnumerable<string> wordstream)
+		{
+			var columnStringBuilder = new StringBuilder();
+
+			var characterMatrix = wordstream
+				.Select(row => row.ToCharArray())
+				.ToArray();
+
+			for (var i = 0; i < characterMatrix.Length; i++)
+			{
+				columnStringBuilder.Length = 0;
+
+				for (var j = 0; j < characterMatrix[i].Length; j++)
+				{
+					columnStringBuilder.Append(characterMatrix[j][i]);
+				}
+
+				_resultSet.UnionWith(_matrix.Where(searchTerm =>
+												 columnStringBuilder.ToString().Contains(searchTerm)));
+			}
+		}
+	}
+}
